1235a7663a059ad5ad274e381b8c7063c9fea2e1
Kyozo Development <kyozodevelopment@gmail.com>
2025-11-13 02:35:30 +0000

    add the icons and + sign on the edge of the circles, keep all the 3 circ

--- src/components/community/feed/create-post-buttons.tsx
+++ src/components/community/feed/create-post-buttons.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Plus, Pencil, Mic, Video } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { CustomButton } from '@/components/ui';
@@ -12,8 +13,6 @@ interface CreatePostButtonsProps {
 }
 
 export const CreatePostButtons: React.FC<CreatePostButtonsProps> = ({ onSelectPostType }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   const buttonConfig = [
     {
       type: 'text' as PostType,
@@ -38,39 +37,51 @@ export const CreatePostButtons: React.FC<CreatePostButtonsProps> = ({ onSelectPo
   return (
     <div className="fixed bottom-8 right-8 z-50">
       <div className="relative flex flex-col items-center gap-2">
-        {isOpen && (
-          <div className="flex flex-col gap-2">
-            {buttonConfig.map((config) => (
-              <Button
-                key={config.type}
-                isIconOnly
-                className="rounded-full w-14 h-14"                
+        {/* Post Type Buttons */}
+        <div className="flex flex-col gap-2">
+          {buttonConfig.map((config) => (
+            <Button
+              key={config.type}
+              isIconOnly
+              className="rounded-full w-14 h-14 relative"
+              style={{
+                backgroundColor: `${config.color}4D`, // 30% opacity
+                borderColor: config.color,
+                color: 'white', // Set icon color to white
+              }}
+              onClick={() => {
+                onSelectPostType(config.type);
+              }}
+            >
+              <config.icon
+                className="w-5 h-5 absolute"
                 style={{
-                  backgroundColor: `${config.color}4D`, // 30% opacity
-                  borderColor: config.color,
-                  color: config.color
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
                 }}
-                onClick={() => {
-                    onSelectPostType(config.type);
-                    setIsOpen(false);
-                }}
-              >
-                <config.icon className="w-6 h-6" />
-              </Button>
-            ))}
-          </div>
-        )}
-        <CustomButton
-            icon={Plus}
-            className="rounded-full w-16 h-16 shadow-lg"
-            onClick={() => setIsOpen(!isOpen)}
+              />
+            </Button>
+          ))}
+        </div>
+
+        {/* Main Add Button */}
+        <div className="relative w-16 h-16">
+          <CustomButton
+            className="rounded-full w-full h-full shadow-lg"
             style={{
-                backgroundColor: '#C170CF',
-                color: 'white'
+              backgroundColor: '#C170CF',
+              color: 'white',
             }}
-        >
-          <Plus className={`w-8 h-8 transition-transform ${isOpen ? 'rotate-45' : ''}`} />
-        </CustomButton>
+          >
+            {/* This button is now purely decorative or could have a default action */}
+          </CustomButton>
+          <div
+            className="absolute top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none"
+          >
+            <Plus className="w-8 h-8 text-white" />
+          </div>
+        </div>
       </div>
     </div>
   );
