ba0b331e686a28606e21ceb98b36e9c65c30bc14
Kyozo Development <kyozodevelopment@gmail.com>
2025-11-18 15:35:54 +0000

    and #CF7770 with /aeropriest/inbox inbox route

--- src/lib/theme-utils.ts
+++ src/lib/theme-utils.ts
@@ -50,7 +50,7 @@ const themeColors: Record<string, { active: string; bg: string }> = {
     overview: { active: '#C170CF', bg: 'rgba(193, 112, 207, 0.3)' },
     members: { active: '#06C4B5', bg: 'rgba(6, 196, 181, 0.3)' },
     broadcast: { active: '#E1B327', bg: 'rgba(225, 179, 39, 0.3)' },
-    inbox: { active: '#CF7770', bg: 'rgba(207, 119, 112, 0.1)' },
+    inbox: { active: '#CF7770', bg: 'rgba(207, 119, 112, 0.3)' },
     feed: { active: '#699FE5', bg: 'rgba(105, 159, 229, 0.3)' },
     ticketing: { active: '#475569', bg: 'rgba(71, 85, 105, 0.1)' },
     integrations: { active: '#0891B2', bg: 'rgba(8, 145, 178, 0.1)' },
