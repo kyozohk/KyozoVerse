63b8342f50902e70160523db7fd382b6510c8ad2
Kyozo Development <kyozodevelopment@gmail.com>
2025-12-11 04:27:04 +0000

    Try fixing this error: `Runtime Error: Error: ./src/app/[handle] You can

--- src/app/[handle]/page.tsx
+++ src/app/[handle]/page.tsx
@@ -1,2 +1,3 @@
-// This file is intentionally left empty and will be renamed to page.tsx.bak to resolve a routing conflict.
+
+// This file is intentionally left empty to resolve a routing conflict.
 // The active page for this route is now located at /src/app/(www)/[handle]/page.tsx.
