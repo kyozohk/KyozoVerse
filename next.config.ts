795020a9e4909a3f0c53b1566ae9541747611b30
Kyozo Development <kyozodevelopment@gmail.com>
2025-12-11 04:00:02 +0000

    Error: Invalid src prop (https://i.pravatar.cc/150?u=X1Axy66xAoZQvBiwZCp

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
       }
     ],
   },
