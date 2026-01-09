57fe69abdbff6d6333671d10b0a0f589765420d5
Kyozo Development <kyozodevelopment@gmail.com>
2026-01-09 05:35:36 +0000

    in the route /ashok/members, when i select an item by checkbox, then it

--- src/components/community/member-list-item.tsx
+++ src/components/community/member-list-item.tsx
@@ -1,10 +1,11 @@
+
 'use client';
 
 import React from 'react';
 import { CommunityMember } from '@/lib/types';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
-import { Checkbox } from '@/components/ui/checkbox';
+import { Checkbox } from '../ui';
 import { Mail, Phone, Edit, MessageCircle, Trash2, X } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
